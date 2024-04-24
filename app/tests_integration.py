06b8e493041d2a1100c4ee99728b8254ede487d5
facularrauri <facularrauri@gmail.com>
2024-04-24 01:19:30 -0300

    Update tests to enable validation when a client is updated

--- app/tests_integration.py
+++ app/tests_integration.py
@@ -82,6 +82,9 @@ class ClientsTest(TestCase):
             data={
                 "id": client.id,
                 "name": "Guido Carrillo",
+                "phone": client.phone,
+                "email": client.email,
+                "address": client.address
             },
         )
 
