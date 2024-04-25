aea476df36b090f679388a6402dde900b6005922
ianbifano <iaanbifano@gmail.com>
2024-04-25 19:16:46 -0300

    ruteo templates

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+    path("mascotas/", view=views.pets_repository, name="pets_repo"),
+    path("mascotas/nuevo/", view=views.pets_form, name="pets_form"),
+    path("mascotas/editar/<int:id>/", view=views.pets_form, name="pets_edit"),
+    path("mascotas/eliminar/", view=views.pets_delete, name="pets_delete"),
 ]
