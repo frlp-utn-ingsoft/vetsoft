c211a177e320046197986cf2ff1de28fede6903c
ianbifano <iaanbifano@gmail.com>
2024-04-25 18:04:39 -0300

    creo el modelo pets

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 def validate_client(data):
@@ -18,6 +19,30 @@ def validate_client(data):
         errors["email"] = "Por favor ingrese un email"
     elif email.count("@") == 0:
         errors["email"] = "Por favor ingrese un email valido"
+
+    return errors
+
+def validate_pet(data):
+    errors = {}
+
+    name = data.get("name", "")
+    breed = data.get("breed", "")
+    birthday = data.get("birthday", "")
+
+    if name == "":
+        errors["name"] = "Por favor ingrese un nombre"
+
+    if breed == "":
+        errors["breed"] = "Por favor ingrese la raza"
+
+    if birthday == "":
+        errors["birthday"] = "Por favor ingrese una fecha"
+    else:
+        birthday_date = datetime.strptime(birthday, "%Y-%m-%d").date()
+        current_date = datetime.now().date()
+
+        if birthday_date > current_date:
+            errors["birthday"] = "La fecha de cumpleaños no puede ser mayor al dia actual"
 
     return errors
 
@@ -52,5 +77,35 @@ class Client(models.Model):
         self.email = client_data.get("email", "") or self.email
         self.phone = client_data.get("phone", "") or self.phone
         self.address = client_data.get("address", "") or self.address
+
+        self.save()
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=300)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def save_pet(cls, pet_data):
+        errors = validate_pet(pet_data)
+
+        if len(errors.keys()) > 0:
+            return False, errors
+
+        Pet.objects.create(
+            name=pet_data.get("name"),
+            breed=pet_data.get("breed"),
+            birthday=pet_data.get("birthday"),
+        )
+
+        return True, None
+
+    def update_pet(self, pet_data):
+        self.name = pet_data.get("name", "") or self.name
+        self.breed = pet_data.get("breed", "") or self.breed
+        self.birthday = pet_data.get("birthday", "") or self.birthday
 
         self.save()
