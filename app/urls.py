d4156d533a232574fd26359cced290a7cf502847
Dalma Muñoz <dalma.mdf@gmail.com>
2024-04-27 14:01:42 -0300

    feat: Agrega vistas a 'medicine' y su repositorio - Agrega vista de formulario y edición en 'views.py' - Crea 'app/templates/medicines/form.html' - Crea 'app/templates/medicines/repository.html' - Agrega URL 'app/urls.py'

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+    path("medicamentos/", view=views.medicines_repository, name="medicines_repo"),
+    path("medicamentos/nuevo/", view=views.medicines_form, name="medicines_form"),
+    path("medicamentos/editar/<int:id>/", view=views.medicines_form, name="medicines_edit"),
+    path("medicamentos/eliminar/", view=views.medicines_delete, name="medicines_delete"),
 ]
