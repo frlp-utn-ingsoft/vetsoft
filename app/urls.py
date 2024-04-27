ea2dc45f28e6d71c97f5f6090723da6579ed36b3
TomaSbert <tomasbret_dx@hotmail.com>
2024-04-27 17:29:58 -0300

    Agregue todas las rutas de Vet, que son, nuevo, editar, eliminar

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+
+    #URLS del modelo Vet
+    path("veterinaria/", view=views.vets_repository, name="vets_repo"),
+    path("veterinaria/nuevo/", view=views.vets_form, name="vets_form"),
+    path("veterinaria/editar/<int:id>/", view=views.vets_form, name="vets_edit"),
+    path("veterinaria/eliminar/", view=views.vets_delete, name="vets_delete"),
 ]
