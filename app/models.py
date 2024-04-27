f404e1cfebcc648b8237ca850c7d7faa20f2fe9c
TomaSbert <tomasbret_dx@hotmail.com>
2024-04-27 17:11:53 -0300

    Agrego el metodo para guardar y validar los datos ingresados de Vet

--- app/models.py
+++ app/models.py
@@ -55,6 +55,34 @@ class Client(models.Model):
 
         self.save()
 
+def validate_vet(data):
+    errors = {}
+
+    name = data.get("name", "")
+    phone = data.get("phone", "")
+    email = data.get("email", "")
+    opening_time = data.get("opening_time", "")
+    closing_time = data.get("closing_time", "")
+
+    if name == "":
+        errors["name"] = "Por favor ingrese un nombre"
+
+    if phone == "":
+        errors["phone"] = "Por favor ingrese un teléfono"
+
+    if email == "":
+        errors["email"] = "Por favor ingrese un email"
+    elif "@" not in email:  # Agreo una verificacion para saber si hay al menos un "@" en el email
+        errors["email"] = "Por favor ingrese un email válido"
+
+    if opening_time == "":
+        errors["opening_time"] = "Por favor ingrese una hora de apertura"
+
+    if closing_time == "":
+        errors["closing_time"] = "Por favor ingrese una hora de cierre"
+
+    return errors
+
 
 class Vet(models.Model):
     name = models.CharField(max_length=100)
@@ -66,3 +94,21 @@ class Vet(models.Model):
 
     def __str__(self):
         return self.name
+    
+    @classmethod
+    def save_vet(cls, vet_data):
+        errors = validate_vet(vet_data)
+
+        if len(errors.keys()) > 0:
+            return False, errors
+
+        Client.objects.create(
+            name=vet_data.get("name"),
+            phone=vet_data.get("phone"),
+            email=vet_data.get("email"),
+            address=vet_data.get("address"),
+            opening_time=vet_data.get("opening_time"),
+            closing_time=vet_data.get("closing_time"), 
+        )
+
+        return True, None
