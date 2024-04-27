cc31a8a9ba3b3479a8418783fe746623aaeff2bf
joaco <joaco.munoz04@gmail.com>
2024-04-27 20:11:49 -0300

    Agrego la funcionalidad de editar un producto

--- app/models.py
+++ app/models.py
@@ -100,8 +100,8 @@ class Product(models.Model):
         return True, None
 
     def update_product(self, product_data):
-        self.name = client_data.get("name", "") or self.name
-        self.type = client_data.get("type", "") or self.type
-        self.price = client_data.get("price", "") or self.price
+        self.name = product_data.get("name", "") or self.name
+        self.type = product_data.get("type", "") or self.type
+        self.price = product_data.get("price", "") or self.price
 
         self.save()
