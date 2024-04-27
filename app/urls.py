0c8a95bb41520526fdbf5e2e99909824c88181ca
Milocco Valentin <valentin-milocco@hotmail.com>
2024-04-27 17:54:26 -0300

    agregue las urls de mascota

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
     path("", view=views.home, name="home"),
+
     path("clientes/", view=views.clients_repository, name="clients_repo"),
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+
+    path("mascotas/", view=views.pets_repository, name="pets_repo"),
+    path("mascotas/nuevo/", view=views.pets_form, name="pets_form"),
+    path("mascotas/editar/<int:id>/", view=views.pets_form, name="pets_edit"),
+    path("mascotas/eliminar/", view=views.pets_delete, name="pets_delete"),
+
 ]
