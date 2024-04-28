e6df47712d6cbc9b4475644cbed1eccff32f3554
JoseLlontop <josellontop100@gmail.com>
2024-04-27 22:37:46 -0300

    feat: Definir URLs para Proveedor -Agrega las URL necesarias para dirigir las solicitudes relacionadas con los proveedores a las vistas correspondientes

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+
+    path("proveedores/", view=views.providers_repository, name="providers_repo"),
+    path("proveedores/nuevo/", view=views.providers_form, name="providers_form"),
+    path("proveedores/editar/<int:id>/", view=views.providers_form, name="providers_edit"),
+    path("proveedores/eliminar/", view=views.providers_delete, name="providers_delete"),
 ]
