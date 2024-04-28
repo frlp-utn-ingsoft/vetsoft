bfecf9576d5535b2c0ce783405b585df7305e047
Nicolás <npavoni10@gmail.com>
2024-04-28 10:49:49 -0300

    Se modifica views.py y urls.py con todo lo relacionado a Medicinas

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+    path("medicines/", view=views.medicines_repository, name="medicines_repo"),
+    path("medicines/nuevo/", view=views.medicines_form, name="medicines_form"),
+    path("medicines/editar/<int:id>/", view=views.medicines_form, name="medicines_edit"),
+    path("medicines/eliminar/", view=views.medicines_delete, name="medicines_delete"),
 ]
