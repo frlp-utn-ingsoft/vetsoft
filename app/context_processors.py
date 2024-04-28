695d7473a6f6f4c987eaa034cd10ef6750ee5e8b
Milocco Valentin <valentin-milocco@hotmail.com>
2024-04-28 12:46:09 -0300

    agrego card de mascota

--- app/context_processors.py
+++ app/context_processors.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
+    {"label": "Mascotas", "href": reverse("pets_repo"), "icon": "bi bi-0-circle"},
 ]
 
 
