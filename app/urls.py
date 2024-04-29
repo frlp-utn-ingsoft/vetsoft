ff726330129f2816c12f45efcd52cf19e9911897
Angel Daniel Marocchi <dam.laplata@gmail.com>
2024-04-28 23:12:05 -0300

    feat: se modifica el archivo urls.py por las nuevas vista para mascotas

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,14 @@ urlpatterns = [
     path("", view=views.home, name="home"),
     path("clientes/", view=views.clients_repository, name="clients_repo"),
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
-    path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
+    path("clientes/editar/<int:id>/",
+         view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+
+    # agregamos las urls de nuestras views para mascotas
+    path("mascotas/", view=views.pets_repository, name="pets_repo"),
+    path("mascotas/nuevo/", view=views.pets_form, name="pets_form"),
+    path("mascotas/editar/<int:id>/", view=views.pets_form, name="pets_edit"),
+    path("mascotas/eliminar/", view=views.pets_delete, name="pets_delete"),
+
 ]
