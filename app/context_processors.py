fed98e94d9af90d153545de0325599640fea79ef
Angel Daniel Marocchi <dam.laplata@gmail.com>
2024-04-28 23:01:07 -0300

    feat: se agrega en el navbar link a Mascotas

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,9 +1,11 @@
 from django.urls import reverse
 
+# agregamos un nuevo contexto para el navbar de mascotas
 links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
-    {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
-]
+    {"label": "Clientes", "href": reverse(
+        "clients_repo"), "icon": "bi bi-people"},
+    {"label": "Mascotas", "href": reverse("pets_repo"), "icon": "bi bi-paw"}]
 
 
 def navbar(request):
