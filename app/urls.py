2bd2836797355a3d250302133b114a2adc951f11
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-05-02 00:51:37 -0300

    ADD(URLs de Product)

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+
+    path("productos/", view=views.products_repository, name="products_repo"),
+    path("productos/nuevo/", view=views.products_form, name="products_for"),
+    path("productos/editar/<int:id>/", view=views.products_form, name="products_edit"),
+    path("productos/eliminar/", view=views.products_delete, name="products_delete"),
+
 ]
