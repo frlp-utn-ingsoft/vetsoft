11c688ad08c2c829d65ada557492e0327dbb1690
pilaracosta96 <acostamariadelpilar3@gmail.com>
2024-05-02 03:29:28 -0300

    home y navbar

--- app/context_processors.py
+++ app/context_processors.py
@@ -4,6 +4,7 @@ links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
     {"label": "Animales", "href": reverse("pets_repo"), "icon": "fas fa-dog"},
+    {"label": "Veterinarios", "href": reverse("vets_repo"), "icon": "fas fa-user-md"},
 ]
 
 def navbar(request):
@@ -23,5 +24,6 @@ def home_items(request):
     items = [
         {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
         {"label": "Animales", "href": reverse("pets_repo"), "icon": "fas fa-dog"},
+        {"label": "Veterinarios", "href": reverse("vets_repo"), "icon": "fas fa-user-md"},
     ]
     return {"home_items": items}
