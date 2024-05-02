b6eb33aba71b2c8156d005f264d345cffd2554e3
Nuria Robledo <nuriarobledo0@gmail.com>
2024-05-02 19:38:56 -0300

    Agregue la fk de client en tabla Pet

--- app/models.py
+++ app/models.py
@@ -126,6 +126,7 @@ class Pet(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=50)
     birthday = models.DateField()
+    client = models.ForeignKey("Client", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
