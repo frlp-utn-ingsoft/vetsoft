41f6a3b21503abc00b7f7d9db460a8adc75b648e
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-05-02 01:21:32 -0300

    ADD(Views de Product)

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Client
-
+from django.views.generic import TemplateView
+from django.views import View
+from .models import Client, Product
 
 def home(request):
     return render(request, "home.html")
@@ -43,3 +44,42 @@ def clients_delete(request):
     client.delete()
 
     return redirect(reverse("clients_repo"))
+
+############################################# PRODUCTS #############################################
+class ProductRepositoryView(TemplateView):
+    template_name = "products/repository.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = Product.objects.all()
+        return context
+
+
+class ProductFormView(View):
+    template_name = "products/form.html"
+
+    def get(self, request, id=None):
+        context = {}
+        if id is not None:
+            context["product"] = get_object_or_404(Product, pk=id)
+        return render(request, self.template_name, context)
+
+    def post(self, request, id=None):
+        product_id = request.POST.get("id", "")
+        if product_id == "":
+            saved, errors = Product.save_product(request.POST)
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            saved, errors = product.update_product(request.POST)
+
+        if saved:
+            return redirect(reverse("products_repo"))
+        return render(request, self.template_name, {"errors": errors, "product": request.POST})
+
+class ProductDeleteView(View):
+    def post(self, request):
+        product_id = request.POST.get("product_id")
+        product = get_object_or_404(Product, pk=int(product_id))
+        product.delete()
+        return redirect(reverse("products_repo"))
+####################################################################################################
