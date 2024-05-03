278cfe37e52923176493b07078dfdf2455ba6a26
Tomas Espinosa <espinosa.tomas@alu.frlp.utn.edu.ar>
2024-05-03 01:27:49 -0300

    ADD(URLS para Vets)

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #CLIENTES
     path("", view=views.home, name="home"),
     path("clientes/", view=views.clients_repository, name="clients_repo"),
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
     path("clientes/eliminar/", view=views.clients_delete, name="clients_delete"),
+
+    #VETERINARIAS
+    path("", view=views.home, name="home"),
+    path("veterinarias/", view=views.vets_repository, name="vets_repo"),
+    path("veterinarias/nuevo/", view=views.vets_form, name="vets_form"),
+    path("veterinarias/editar/<int:id>/", view=views.vets_form, name="vets_edit"),
+    path("veterinarias/eliminar/", view=views.vets_delete, name="vets_delete"),
 ]
