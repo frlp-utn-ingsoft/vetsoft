e3245ddbde6c0007e48c49486ce1a684e28d2a13
Tomas Espinosa <espinosa.tomas@alu.frlp.utn.edu.ar>
2024-05-03 01:23:21 -0300

    ADD(Model de Vet)

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+#CLIENTES
 def validate_client(data):
     errors = {}
 
@@ -52,5 +52,57 @@ class Client(models.Model):
         self.email = client_data.get("email", "") or self.email
         self.phone = client_data.get("phone", "") or self.phone
         self.address = client_data.get("address", "") or self.address
+
+        self.save()
+
+#VETS
+def validate_vet(data):
+    errors = {}
+
+    name = data.get("name", "")
+    phone = data.get("phone", "")
+    email = data.get("email", "")
+
+    if name == "":
+        errors["name"] = "Por favor ingrese un nombre"
+
+    if phone == "":
+        errors["phone"] = "Por favor ingrese un teléfono"
+
+    if email == "":
+        errors["email"] = "Por favor ingrese un email"
+    elif email.count("@") == 0:
+        errors["email"] = "Por favor ingrese un email valido"
+
+    return errors
+
+
+class Vet(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def save_vet(cls, vet_data):
+        errors = validate_vet(vet_data)
+
+        if len(errors.keys()) > 0:
+            return False, errors
+
+        Vet.objects.create(
+            name=vet_data.get("name"),
+            phone=vet_data.get("phone"),
+            email=vet_data.get("email"),
+        )
+
+        return True, None
+
+    def update_vet(self, vet_data):
+        self.name = vet_data.get("name", "") or self.name
+        self.email = vet_data.get("email", "") or self.email
+        self.phone = vet_data.get("phone", "") or self.phone
 
         self.save()
