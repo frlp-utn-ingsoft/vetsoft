9f62c2596f3431d7d0bd470126435fe28f3bbab0
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-05-03 14:15:31 -0300

    ADD(URLs Medicine)

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #CLIENTES
+    # Ruta raíz
     path("", view=views.home, name="home"),
+
+    #CLIENTES
     path("clientes/", view=views.clients_repository, name="clients_repo"),
     path("clientes/nuevo/", view=views.clients_form, name="clients_form"),
     path("clientes/editar/<int:id>/", view=views.clients_form, name="clients_edit"),
@@ -16,9 +18,15 @@ urlpatterns = [
     path("productos/eliminar/", views.ProductDeleteView.as_view(), name="products_delete"),
 
     #VETERINARIAS
-    path("", view=views.home, name="home"),
     path("veterinarias/", view=views.vets_repository, name="vets_repo"),
     path("veterinarias/nuevo/", view=views.vets_form, name="vets_form"),
     path("veterinarias/editar/<int:id>/", view=views.vets_form, name="vets_edit"),
     path("veterinarias/eliminar/", view=views.vets_delete, name="vets_delete"),
+
+    #MEDICAMENTOS
+    path("medicamentos/", views.MedicineRepositoryView.as_view(), name="medicines_repo"),
+    path("medicamentos/nuevo/", views.MedicineFormView.as_view(), name="medicines_create"),
+    path("medicamentos/editar/<int:id>/", views.MedicineFormView.as_view(), name="medicines_edit"),
+    path("medicamentos/eliminar/", views.MedicineDeleteView.as_view(), name="medicines_delete"),
+
 ]
