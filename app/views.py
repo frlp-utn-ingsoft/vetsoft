64a4b6127920c06a9d5da7b76116542e05f80372
Lucas Martinez <lucas.olimpi@gmail.com>
2024-05-02 21:07:20 -0300

    se crea la clase product en app/views.py para luego crear las carpetas correspondientes para los HTMLs

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Client, Pet, Medicine, Vet
+from .models import Client, Pet, Medicine, Vet, Product
 
 
 def home(request):
@@ -153,3 +153,41 @@ def vets_delete(request):
     vet.delete()
 
     return redirect(reverse("vets_repo"))
+
+def product_repository(request):
+    products = Product.objects.all()
+    return render(request, "products/repository.html", {"products": products})
+
+
+def products_form(request, id=None):
+    if request.method == "POST":
+        product_id = request.POST.get("id", "")
+        errors = {}
+        saved = True
+
+        if product_id == "":
+            saved, errors = Product.save_product(request.POST)
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            product.update_product(request.POST)
+
+        if saved:
+            return redirect(reverse("product_repo"))
+
+        return render(
+            request, "products/form.html", {"errors": errors, "product": request.POST}
+        )
+
+    product = None
+    if id is not None:
+        product = get_object_or_404(Product, pk=id)
+
+    return render(request, "products/form.html", {"product": product})
+
+
+def product_delete(request):
+    product_id = request.POST.get("product_id")
+    product = get_object_or_404(Product, pk=int(product_id))
+    product.delete()
+
+    return redirect(reverse("products_repo"))
