7d6ba111cf7cfc1c3199e7bec1f6160da370d3a2
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2024-05-03 05:26:29 -0300

    [Parte 1.4.12] Agregue relacion m:m entre product y client

--- app/models.py
+++ app/models.py
@@ -49,40 +49,6 @@ def validate_product(data):
     return errors
                 
 
-
-class Client(models.Model):
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
-    email = models.EmailField()
-    address = models.CharField(max_length=100, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    @classmethod
-    def save_client(cls, client_data):
-        errors = validate_client(client_data)
-
-        if len(errors.keys()) > 0:
-            return False, errors
-
-        Client.objects.create(
-            name=client_data.get("name"),
-            phone=client_data.get("phone"),
-            email=client_data.get("email"),
-            address=client_data.get("address"),
-        )
-
-        return True, None
-
-    def update_client(self, client_data):
-        self.name = client_data.get("name", "") or self.name
-        self.email = client_data.get("email", "") or self.email
-        self.phone = client_data.get("phone", "") or self.phone
-        self.address = client_data.get("address", "") or self.address
-
-        self.save()
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
@@ -114,6 +80,41 @@ class Product(models.Model):
             pass
 
         self.save()
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.CharField(max_length=100, blank=True)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def save_client(cls, client_data):
+        errors = validate_client(client_data)
+
+        if len(errors.keys()) > 0:
+            return False, errors
+
+        Client.objects.create(
+            name=client_data.get("name"),
+            phone=client_data.get("phone"),
+            email=client_data.get("email"),
+            address=client_data.get("address"),
+        )
+
+        return True, None
+
+    def update_client(self, client_data):
+        self.name = client_data.get("name", "") or self.name
+        self.email = client_data.get("email", "") or self.email
+        self.phone = client_data.get("phone", "") or self.phone
+        self.address = client_data.get("address", "") or self.address
+
+        self.save()
+
 
 class Medicine(models.Model):
     name = models.CharField(max_length=100)
