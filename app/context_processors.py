652ddf75841885bb458eb6799d625ef025d0f5bc
Tomas Espinosa <espinosa.tomas@alu.frlp.utn.edu.ar>
2024-05-06 18:27:37 -0300

    FIX(Agrego botón Proveedores para navbar)

--- app/context_processors.py
+++ app/context_processors.py
@@ -3,9 +3,10 @@ from django.urls import reverse
 links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
-    {"label": "Productos", "href": reverse("products_repo"), "icon": "bi bi-basket"},
     {"label": "Veterinarias", "href": reverse("vets_repo"), "icon": "bi bi-shop"},
+    {"label": "Productos", "href": reverse("products_repo"), "icon": "bi bi-basket"},
     {"label": "Medicamentos", "href": reverse("medicines_repo"), "icon": "bi bi-capsule"},
+    {"label": "Proveedores", "href": reverse("providers_repo"), "icon": "bi bi-truck"},
     {"label": "Mascotas", "href": reverse("pets_repo"), "icon": "bi bi-chat-heart"},
 ]
 
