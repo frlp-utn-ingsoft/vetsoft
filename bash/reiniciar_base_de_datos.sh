6a046f95b4f2bcc369e426a5b737bbdd8efbb8f6
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-05-10 01:12:40 -0300

    FIX(Se ajusto el script para reiniciar la base de datos)

--- bash/reiniciar_base_de_datos.sh
+++ bash/reiniciar_base_de_datos.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Borra la base, reinicia las migraciones y corre los datos aleatorios para pruebas de datos
 cd ..;
-rm db.sqlite3;
-python manage.py migrate;
+source ".env";
+rm "$DB_NAME" && echo "Se borro la base de datos '$DB_NAME'";
+python3 manage.py migrate && echo "se corrieron las migraciones de la base de datos";
+python3 manage.py loaddata fixtures/data.json && echo "Se cargaron datos aleatorios";
