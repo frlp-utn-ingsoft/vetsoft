45ec3fc8d8f06fb9784fcf82b403bb0105442816
Sebastian Gonzales <sebastian.gt.2002@gmail.com>
2024-05-18 03:13:54 -0300

    feat: Agrego test unitario para validar rango de dosis

--- app/tests_unit.py
+++ app/tests_unit.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from app.models import Client
+from app.models import Client, validate_medicine
 
 
 class ClientModelTest(TestCase):
@@ -57,3 +57,41 @@ class ClientModelTest(TestCase):
         client_updated = Client.objects.get(pk=1)
 
         self.assertEqual(client_updated.phone, "221555232")
+
+class MedicineModelTest(TestCase):
+    def test_medicine_dose_validation_in_range_1_to_10(self):
+        # Crear un diccionario con los datos del medicamento
+        medicine_data = {
+            "name": "Test Medicine",
+            "description": "Test Description",
+            "dose": -10 
+        }
+
+        # Llamar a la función de validación del medicamento
+        errors = validate_medicine(medicine_data)
+
+        # Comprobar que hay un error de validación en el campo 'dose'
+        self.assertIn("dose", errors)
+        self.assertEqual(errors["dose"], "La dosis debe estar en un rango de 1 a 10")
+
+        # Probar con otra dosis inválida, superando el rango
+        medicine_data["dose"] = 15
+        errors = validate_medicine(medicine_data)
+
+        # Comprobar que hay un error de validación en el campo 'dose'
+        self.assertIn("dose", errors)
+        self.assertEqual(errors["dose"], "La dosis debe estar en un rango de 1 a 10")
+
+        # Probar con otra dosis válida, dentro el rango
+        medicine_data["dose"] = 10
+        errors = validate_medicine(medicine_data)
+
+        # Comprobar que no hay errores de validación
+        self.assertNotIn("dose", errors)
+
+        # Probar con otra dosis válida, dentro el rango
+        medicine_data["dose"] = 5
+        errors = validate_medicine(medicine_data)
+
+        # Comprobar que no hay errores de validación
+        self.assertNotIn("dose", errors)
