c9283bd8616e1675f9c0a0328c9a8979e0c9c98a
Juan Ignacio Frangolini <nachofrangolini@hotmail.com>
2024-05-19 14:09:52 -0300

    feat: agrego campo de dirección al modelo de Proveedor

--- app/models.py
+++ app/models.py
@@ -231,6 +231,7 @@ def validate_provider(data):
 
     name = data.get("name", "")
     email = data.get("email", "")
+    address = data.get("address", "")
 
     if name == "":
         errors["name"] = "Por favor ingrese un nombre"
@@ -240,12 +241,15 @@ def validate_provider(data):
     elif email.count("@") == 0:
         errors["email"] = "Por favor ingrese un email valido"
 
-    return errors
+    if address == "":
+        errors["address"] = "Por favor ingrese una dirección"
 
+    return errors
 
 class Provider(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
@@ -260,6 +264,7 @@ class Provider(models.Model):
         Provider.objects.create(
             name=provider_data.get("name"),
             email=provider_data.get("email"),
+            address=provider_data.get("address"),
         )
 
         return True, None
@@ -267,8 +272,10 @@ class Provider(models.Model):
     def update_provider(self, provider_data):
         self.name = provider_data.get("name", "") or self.name
         self.email = provider_data.get("email", "") or self.email
+        self.address = provider_data.get("address", "") or self.address
 
         self.save()
+
 
  ##---------vets----------   
 def validate_vet(data):
