f5fdcda99179097554947275de497d9ba8c7e65c
facularrauri <facularrauri@gmail.com>
2024-05-22 20:21:02 -0300

    add app for serverless

--- vetsoft/settings.py
+++ vetsoft/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 # Application definition
 
 INSTALLED_APPS = [
-    "app.apps.AppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app"
 ]
 
 MIDDLEWARE = [
