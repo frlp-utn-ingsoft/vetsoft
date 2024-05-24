8bd54cc9ac2db23e907927d2140e13c10a08826b
Santillan Bruno <brunosaantillan@gmail.com>
2024-05-24 17:35:44 -0300

    test(test_unit.py):Se agrego test de unidad para la creacion de una Mascota

--- app/tests_unit.py
+++ app/tests_unit.py
@@ -1,5 +1,6 @@
+from datetime import date
 from django.test import TestCase
-from app.models import Client
+from app.models import Client, Pet
 
 
 class ClientModelTest(TestCase):
@@ -56,4 +57,32 @@ class ClientModelTest(TestCase):
 
         client_updated = Client.objects.get(pk=1)
 
-        self.assertEqual(client_updated.phone, "221555232")
+        self.assertEqual(client_updated.phone, "221555232")    
+
+class PetModelTest(TestCase, ):
+    def test_can_create_and_get_pet(self):
+        Client.save_client(
+            {
+                "name": "Juan Sebastian Veron",
+                "phone": "221555232",
+                "address": "13 y 44",
+                "email": "brujita75@hotmail.com",
+            }
+        )
+        Pet.save_pet(
+            {
+                "name": "Loki",
+                "breed": "Border Collie",
+                "birthday": date(2024,5,5),
+                "weight": 10,
+                "client":1
+            }
+        )
+        pets = Pet.objects.all()
+        self.assertEqual(len(pets), 1)
+
+        self.assertEqual(pets[0].name, "Loki")
+        self.assertEqual(pets[0].breed, "Border Collie")
+        self.assertEqual(pets[0].birthday, date(2024,5,5))
+        self.assertEqual(pets[0].weight, 10)
+        self.assertEqual(pets[0].client, Client.objects.get(pk=1))
