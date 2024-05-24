09aad4f5965da0977fc502e2b1dccbd5c02747c1
Santillan Bruno <brunosaantillan@gmail.com>
2024-05-24 13:07:28 -0300

    feat(models.py):Se agrego el atributo weight en el update_pet()

--- app/models.py
+++ app/models.py
@@ -194,6 +194,7 @@ class Pet(models.Model):
         self.name = pet_data.get("name","") or self.name
         self.breed = pet_data.get("breed", "") or self.breed
         self.birthday = pet_data.get("birthday", "") or self.birthday
+        self.weight = pet_data.get("weight", "") or self.weight
 
         self.save()    
 
