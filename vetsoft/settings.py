561d098fe4289e4f1694b614356fa1dc968bba24
renatacatelli <renatacatelli15@gmail.com>
2024-05-25 15:31:12 -0300

    Agrego el archivo settings para funcionamiento de tests de unidad e integracion

--- vetsoft/settings.py
+++ vetsoft/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = "vetsoft.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.getenv("DB_NAME", str(BASE_DIR / "db.sqlite3")),
     }
 }
 
