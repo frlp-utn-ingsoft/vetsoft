f300a4ad759b8630fedbdd4bef09963b1a1b2189
ianbifano <iaanbifano@gmail.com>
2024-05-25 17:58:26 -0300

    feat/Tests valida fecha nacimiento

---
+++ app/tests/test_pet_birthday.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from app.models import Pet
+from datetime import datetime
+
+class PetBirthdayTest(TestCase):
+
+    def test_create_pet(self):
+        pet = Pet.objects.create(name="Test Pet", breed="Test Breed", birthday=datetime.strptime("2024-05-06", "%Y-%m-%d").date())
+        self.assertEqual(pet.name, "Test Pet")
+        self.assertEqual(pet.breed, "Test Breed")
+        self.assertEqual(pet.birthday, datetime.strptime("2024-05-06", "%Y-%m-%d").date())
+
+    def test_validate_birthday_date(self):
+        pet_birthday = datetime.strptime("2024-05-06", "%Y-%m-%d").date()
+        if pet_birthday > datetime.now().date():
+            raise ValueError("La fecha de cumpleaños no puede ser mayor al dia actual.")
+        
+        pet = Pet.objects.create(name="Test Pet", breed="Test Breed", birthday=pet_birthday)
+        self.assertEqual(pet.name, "Test Pet")
+        self.assertEqual(pet.breed, "Test Breed")
+        self.assertEqual(pet.birthday, pet_birthday)
+        self.assertTrue(pet.birthday < datetime.now().date(), "La fecha de cumpleaños no puede ser mayor al dia actual.")
