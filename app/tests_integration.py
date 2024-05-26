faf4dc61f4f40117e71b08bf8ecd1d2781441c1c
Nicolas Pieroni <pieroni.nico@gmail.com>
2024-05-26 20:11:02 -0300

    ADD(test de integración para forms)

--- app/tests_integration.py
+++ app/tests_integration.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import reverse
-from app.models import Client
+from app.models import Client, Medicine, Pet, Product
 
 
 class HomePageTest(TestCase):
@@ -8,6 +8,16 @@ class HomePageTest(TestCase):
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "home.html")
 
+class FormPagesTest(TestCase):
+    def test_forms_template(self):
+        response_clients_form = self.client.get(reverse("clients_form"))
+        response_vets_form = self.vets.get(reverse("vets_form"))
+        response_providers_form = self.providers.get(reverse("providers_form"))
+        response_pets_form = self.pets.get(reverse("pets_form"))
+        self.assertTemplateUsed(response_clients_form, "clients_form")
+        self.assertTemplateUsed(response_vets_form, "vets_form")
+        self.assertTemplateUsed(response_providers_form, "providers_form")
+        self.assertTemplateUsed(response_pets_form, "pets_form")
 
 class ClientsTest(TestCase):
     def test_repo_use_repo_template(self):
