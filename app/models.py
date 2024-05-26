f1014cd3eb45fe5628e7b0aaa4f62f7c0794b129
Katerina Mariescurrena <katyfacultad@gmail.com>
2024-05-26 15:25:22 -0300

    feat (template):Se agrego atributo direccion

--- app/models.py
+++ app/models.py
@@ -91,6 +91,7 @@ def validate_pet(data):
 class Provider(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    address = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.name
@@ -105,12 +106,14 @@ class Provider(models.Model):
         Provider.objects.create(
             name=provider_data.get("name"),
             email=provider_data.get("email"),
+            address=provider_data.get("address"),
         )
         return True, None
     
     def update_provider(self, provider_data):
         self.name = provider_data.get("name", "") or self.name
         self.email = provider_data.get("email", "") or self.email
+        self.address = provider_data.get("address", "") or self.address
 
         self.save()
     
