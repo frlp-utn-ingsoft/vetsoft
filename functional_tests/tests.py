204863486bc52d3787cdf18ed5a9cb75a11b2d11
Luciano Garcia Montes <xzargothrax@gmail.com>
2024-05-26 21:58:24 -0300

    fix: Nuevo intento de resolver merge tras errores

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -244,22 +244,6 @@ class ClientCreateEditTestCase(PlaywrightTestCase):
             "href", reverse("clients_edit", kwargs={"id": client.id})
         )
 
-class AddMedicine(PlaywrightTestCase):
-    def test_add_new_medicine(self):
-        self.page.goto(f"{self.live_server_url}{reverse('medicines_form')}")
-
-        expect(self.page.get_by_role("form")).to_be_visible()
-
-        self.page.get_by_label("Nombre").fill("Bayaspirina")
-        self.page.get_by_label("Descripción").fill("Para dolores de cabeza")
-        self.page.get_by_label("Dosis").fill("1")
-
-        self.page.get_by_role("button", name="Guardar").click()
-
-        expect(self.page.get_by_text("Bayaspirina")).to_be_visible()
-        expect(self.page.get_by_text("Para dolores de cabeza")).to_be_visible()
-        expect(self.page.get_by_text("1")).to_be_visible()
-
 class AddPet(PlaywrightTestCase):
     def test_add_new_pet(self):
         self.page.goto(f"{self.live_server_url}{reverse('pets_form')}")
@@ -324,4 +308,35 @@ class AddProduct(PlaywrightTestCase):
         expect(self.page.get_by_text("10")).to_be_visible()
         expect(self.page.get_by_text("15")).to_be_visible()
 
+class AddMedicine(PlaywrightTestCase):
+    def test_add_new_medicine(self):
+        self.page.goto(f"{self.live_server_url}{reverse('medicines_form')}")
 
+        expect(self.page.get_by_role("form")).to_be_visible()
+
+        self.page.get_by_label("Nombre").fill("Bayaspirina")
+        self.page.get_by_label("Descripción").fill("Para dolores de cabeza")
+        self.page.get_by_label("Dosis").fill("1")
+
+        self.page.get_by_role("button", name="Guardar").click()
+
+        expect(self.page.get_by_text("Bayaspirina")).to_be_visible()
+        expect(self.page.get_by_text("Para dolores de cabeza")).to_be_visible()
+        expect(self.page.get_by_text("1")).to_be_visible()
+
+    def test_medicine_dose_range(self):
+        self.page.goto(f"{self.live_server_url}{reverse('medicines_form')}")
+
+        expect(self.page.get_by_role("form")).to_be_visible()
+
+        self.page.get_by_label("Nombre").fill("Medicina 1")
+        self.page.get_by_label("Descripción").fill("Descripción de medicina 1")
+        self.page.get_by_label("Dosis").fill("0")
+
+        self.page.get_by_role("button", name="Guardar").click()
+        expect(self.page.get_by_text("La dosis debe ser entre 1 y 10")).to_be_visible()
+
+        self.page.get_by_label("Dosis").fill("12")
+
+        self.page.get_by_role("button", name="Guardar").click()
+        expect(self.page.get_by_text("La dosis debe ser entre 1 y 10")).to_be_visible()
