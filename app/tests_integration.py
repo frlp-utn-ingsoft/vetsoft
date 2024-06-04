c117f87d4545cbe4f51d2671a68fe0fc6ae3a7ce
Nuria Robledo <nuriarobledo0@gmail.com>
2024-06-04 13:36:02 -0300

    feat: - Agrego test para verificar que el mail termine con el formato correcto

--- app/tests_integration.py
+++ app/tests_integration.py
@@ -70,7 +70,19 @@ class ClientsTest(TestCase):
 
         self.assertContains(response, "Por favor ingrese un email válido")
 
-   
+    def test_validate_email_ends_with_vetsoft_com(self):
+        response = self.client.post(
+            reverse("clients_form"),
+            data={
+                "name": "Rosario Central",
+                "phone": "4444444",
+                "address": "13 y 44",
+                "email": "rositac@gmail.com",
+            },
+        )
+
+        self.assertContains(response, "El email debe terminar con @vetsoft.com")    
+
     def test_edit_user_with_valid_data(self):
         client = Client.objects.create(
             name="Juan Sebastián Veron",
